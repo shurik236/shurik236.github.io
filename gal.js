fe078884a7303d318d0a6d20eb16c7aaa909723f
Колиниченко Александр <7e0d4547d5e940b9@naumen.ru>
2016-12-18 01:36:14 +0500

    gallery is ready

--- gal.js
+++ gal.js
@@ -1,89 +1,144 @@
-var arr = [
-    "gallery/1.jpg",
-    "gallery/2.jpg",
-    "gallery/3.jpg",
-    "gallery/4.jpg",
-    "gallery/5.jpg",
-    "gallery/6.jpg",
-    "gallery/7.jpg",
-    "gallery/8.jpg",
-    "gallery/9.jpg",
-    "gallery/10.jpg",
-    "gallery/11.jpg",
-    "gallery/12.jpg"
-];
-var to = -1;  // Счетчик, указывающий на текущую картинки
-
-function right_arrow() // Открытие следующей картинки(движение вправо)
-{
-    var obj = document.getElementById("img");
-    if (to < arr.length-1)  to++;
-    else
-        to = 0;
-    obj.src = arr[to];
+function backToPrev() {
+    window.location.href = '#gallery-prev';
+    hideHelp();
 }
 
-function left_arrow()
-{
-    var obj = document.getElementById("img");
-    if (to > 0) to--;
-    else
-        to = arr.length-1;
-    obj.src = arr[to];
+function hideHelp() {
+    document.getElementById("help").style.display = 'none';
+    document.getElementById("help").style.visibility = 'hidden';
 }
 
+function showHelp() {
+    document.getElementById("help").style.display = 'block';
+    document.getElementById("help").style.visibility = 'visible';
+}
 
-function load()   // Ф-ция загрузки "сохраненной" картинки
-{
-    for (var i = 0 ; i < arr.length; i++)
-    {
-        if (arr[i] == cook_val)   // Как только встретилась
-        {
-            document.getElementById("img").src = arr[i];   // Загружаем картинку
-            to = i;  // Задаем текущее значение счетчику
-            break; // выходим
+function isVisible(objectId) {
+    var object = window.document.getElementById(objectId);
+    if (!object)
+        return;
+    if (object.currentStyle) {
+        return (object.currentStyle['display'] === 'block' || object.currentStyle['visibility'] === 'visible');
+    }
+    else if (window.getComputedStyle) {
+        var style = window.getComputedStyle(object, "");
+        return (style.display === 'block' || style.visibility === 'visible');
+    }
+
+    return false;
+
+}
+
+function pictureIsShown() {
+    var pictureShown = 0;
+    for (var i = 1; i < 13; i++) {
+        if (isVisible('image' + i)) {
+            pictureShown = i;
         }
     }
+
+    return pictureShown;
 }
 
-function revealSpinner() {
-    document.getElementById('spinner').style.visibility = 'visible';
-}
+function galleryKeyHandler(event) {
+    if (event === undefined) {
+        event = window.event;
+    }
 
-function concealSpinner() {
-    document.getElementById('spinner').style.visibility = 'hidden';
-}
+    if (event.preventDefault) {
+        event.preventDefault();
+    }
+    event.returnValue = false;
+    console.log(event.returnValue);
 
-function setPicture(img)
-{
-    for (var i = 0; i<arr.length; i++) {
-        if (arr[i] === img) {
-            document.getElementById("img").src = arr[i];
-            to = i;
-            break;
+
+    var current = pictureIsShown();
+    if (current === 0)
+        return;
+    var code;
+
+    if (event.key !== undefined) {
+        code = event.key;
+    } else if (event.keyCode !== undefined) {
+        code = event.keyCode;
+    }
+    console.log(code);
+
+    if (code === 'Esc' || code === 'Escape') {
+        backToPrev();
+    }
+    if (code === 'F1') {
+        var help = document.getElementById('help');
+        if (isVisible('help')){
+            hideHelp();
+        }
+        else {
+            showHelp();
         }
     }
-}
-
-function showPicture(img) {
-    setPicture(img);
-    revealSpinner();
-    var state = {
-        selectedImage: img,
-        spinnerVisible: true
-    };
-    var title = "My portfolio";
-    var path = '/' + img;
-    window.history.pushState(state, title, path);
-}
-
-function onPopstate(event) {
-    var state = event.originalEvent.state;
-
-    if (state.selectedImage && state.spinnerVisible) {
-        showPicture(state.selectedImage);
+    if (code === 'ArrowLeft' || code === 'Left') {
+        current--;
+        if (current === 0)
+            current = 12;
+        window.location.href = '#image' + current;
     }
-    else
-        concealSpinner();
-
+    if (code === 'ArrowRight' || code === 'Right') {
+        current++;
+        if (current === 13)
+            current = 1;
+        window.location.href = '#image' + current;
+    }
+    
+    return false;
 }
+
+
+function changeIntroBackground(imgURL) {
+
+    if (imgURL) {
+        document.getElementById("intro").style.backgroundImage = 'url(' + imgURL + ')';
+        if (getCookie('background')) {
+            deleteCookie('background');
+        }
+        setCookie('background', imgURL, 1);
+    }
+}
+
+
+function load() {
+    var bgURL = getCookie('background');
+    if (bgURL)
+        changeIntroBackground(bgURL);
+    else
+        changeIntroBackground('intro.jpg');
+}
+
+
+function setCookie(cname, cvalue, exdays) {
+    var d = new Date();
+    d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+    var expires = "expires="+d.toUTCString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+
+function getCookie(cname) {
+    var name = cname + "=";
+    var ca = document.cookie.split(';');
+    for(var i = 0; i < ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
+}
+
+function deleteCookie( name ) {
+    document.cookie = name + '=; expires=Thu, 01 Jan 1970 00:00:01 GMT;';
+}
+
+window.document.onkeydown = function(event) {galleryKeyHandler(event)};
+window.document.onhelp = function(event) {return false};
